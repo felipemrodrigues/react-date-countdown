5792cd6849c0b28fbea51d00dda1249c6fb03e96
Felipe Rodrigues <feliperodrigues@gmail.com>
2018-07-28 14:56:19 +0100

    remove chai dependency + remove default test

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,9 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { expect } from 'chai'
 
 import Countdown from './'
-
-it('should be a function', () => {
-    expect(Countdown).to.be.a('function')
-})
