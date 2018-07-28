d315851391824d839649d55171cb2959ddc16eca
Felipe Rodrigues <feliperodrigues@gmail.com>
2018-07-28 18:36:59 +0100

    test if element is rendering

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,4 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
 import Countdown from './'
+
+configure({ adapter: new Adapter() })
+
+describe('<Countdown />', () => {
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = shallow(<Countdown  />)
+    })
+
+    it('renders div if finalDate is true', () => {
+        wrapper.setProps({ finalDate: '2018-07-16T00:00:00Z' })
+        expect(wrapper.find('div')).toHaveLength(1)
+    })
+})
